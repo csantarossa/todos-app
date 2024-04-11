f89501fc1562c156ff4129d0f285d13290c30938
COREY SANTAROSSA <santarossacorey@gmail.com>
2024-04-11 23:19:08 +1000

    merged build to server dir

--- server/index.js
+++ server/index.js
@@ -3,10 +3,15 @@ const mongodb = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const { connectToMongoDB } = require("./database");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build/index.html"));
+});
 
 const router = require("./routes");
 
